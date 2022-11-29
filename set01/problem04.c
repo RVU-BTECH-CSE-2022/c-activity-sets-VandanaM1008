6cbd342fa146938d6b1a02530363047dd75e7607
Vandana  M <115770079+VandanaM1008@users.noreply.github.com>
2022-11-29 04:38:55 +0000

    somethin

---
+++ set01/problem04.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(void)
+{
+  int a,b,sum;
+  printf("Enter the first number\n");
+  scanf("%d" ,&a);
+  printf("Enter the second number\n");
+  scanf("%d" ,&b);
+  sum=sum_func(&a,&b);
+  printf("The sum of the 2 numbers is %d",sum);
+  return(0);
+}
+sum_func(int *a,int *b)
+{
+  int sum=*a+*b;
+  return sum;
+}
---
+++ set01/problem09.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    double n, x, y;
+    printf("Enter a number: ");
+    scanf("%lf", &n);    
+    x = n;
+    y = 1;
+    while (x - y > 0.000001)
+    {
+        x = (x + y)/2;
+        y = n/x;
+    }
+    printf("Square root of %lf = %lf", n, x);
+    return 0;
+}
